e633746877e83bc099cc3a8deb53e7789a7f83ec
DoVietAnh <anhdv@projectkit.net>
2017-05-05 15:40:52 +0700

    #7 fix scoreboard module

--- modules/scoreboard.js
+++ modules/scoreboard.js
@@ -174,7 +174,7 @@ module.exports =
                 }
                 var $ = cheerio.load(rawData);
                 var lstClass = $('select[name=lstClass]');
-                var currentTerm = this.getCurrentTerm();
+                var currentTerm = this.getCurrentTerm(new Date());
                 logger.info('current term');
                 logger.info(JSON.stringify(currentTerm));
                 $('option', lstClass).each((opt_idx, opt) => {

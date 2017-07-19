bd0c052a6bcad6973c10a4d6706e2dffd1d23b68
globalfarmer <dovietanh.95@gmail.com>
2017-07-19 16:53:30 +0700

    #53 change muckup pack

--- mockup/pack.js
+++ mockup/pack.js
@@ -1,10 +1,39 @@
+student_pack // expireAfterSeconds 2 hours
 {
     "_id": "",
-    "type": "slot/finaltest/session",
-    "config": {
+    "config":
+    {
         "options": {},
-        "params": {}
+        "params": {},
+        "term": "2016-2017-2",
     },
-    "status": "pending/done",
-    "version": "",
+    "version": "createdAt: Date"
+}
+
+slot_pack // expireAfterSeconds x hours
+{
+    "_id": "",
+    "config":
+    {
+        "options": {},
+        "params": {},
+        "term": "2016-2017-2"
+    },
+    "version": "createdAt: Date"
+}
+
+finaltest_pack // expireAfterSeconds x hours
+{
+    "_id": "",
+    "config":
+    {
+        "options": {},
+        "params": {},
+        "term": "2016-2017-2"
+    },
+    "version": "createdAt: Date"
+}
+
+scoreboard_pack
+{    
 }

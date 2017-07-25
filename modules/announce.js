8a7b6281bc958f9a590f966bd311189e2f453331
globalfarmer <dovietanh.95@gmail.com>
2017-07-25 08:33:27 +0700

    #56 change href -> link

--- modules/announce.js
+++ modules/announce.js
@@ -32,11 +32,11 @@ AnnounceCrawler.prototype.parse = function() {
     this.hrefArr = [];
     $('.views-field-title').each((col, item) => {
         var announce = {
-            href: $('a', item).attr('href'),
+            link: $('a', item).attr('href'),
             name: $('a', item).attr('title'),
         }
-        this.hrefArr.push(announce.href);
-        this.announces[announce.href] = announce;
+        this.hrefArr.push(announce.link);
+        this.announces[announce.link] = announce;
     });
     if( !production ) {
         logger.log( this.annouces );
@@ -56,7 +56,7 @@ AnnounceCrawler.prototype.saveToDB = function() {
         }
         else
         {
-            for(item of items) delete this.announces[item.href];
+            for(item of items) delete this.announces[item.link];
             var bulk = iNoodle.db.collection('announce').initializeUnorderedBulkOp();
             for(href of this.hrefArr.reverse()) {
                 if( this.announces[href] ) {
